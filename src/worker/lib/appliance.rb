ae0fb6e1975dd7fffee8e45bf96bd990753bd9a6
Jaime Melis <jmelis@opennebula.org>
2014-01-15 16:52:52 +0100

    Refactor OVF parser and Appliance ruby library

---
+++ src/worker/lib/appliance.rb
@@ -0,0 +1,134 @@
+################################################################################
+# ApplianceFile
+################################################################################
+
+class ApplianceFile
+    def initialize(hash = nil)
+        @hash = hash || Hash.new
+    end
+
+    # Hash implementation
+    def to_hash; @hash; end
+    def [](key); @hash[key]; end
+    def []=(key, value); @hash[key] = value; end
+
+    # Static methods
+    def self.register(hash)
+        source = hash[:path]
+        name   = hash[:name] || source.split("/")[-1]
+        target = hash[:target]
+
+        uuid = UUIDTools::UUID.random_create.to_s
+
+        path = File.join(CONF[:repo], uuid)
+        url  = File.join(CONF[:base_uri], uuid)
+
+        FileUtils.mv(source, path)
+
+        digests = self.digests(path)
+
+        self.new({
+            "name" => name,
+            "url"  => url,
+            "size" => File.size?(path).to_s,
+            "md5"  => digests[:md5],
+            "sha1" => digests[:sha1]
+        })
+    end
+
+    def self.digests(path)
+        md5sum  = Digest::MD5.new
+        sha1sum = Digest::SHA1.new
+
+        File.open(path, 'rb') do |io|
+            buffer = String.new
+            while io.read(4096, buffer)
+                md5sum.update(buffer)
+                sha1sum.update(buffer)
+            end
+        end
+
+        {
+            :md5  => md5sum.to_s,
+            :sha1 => sha1sum.to_s
+        }
+    end
+end
+
+################################################################################
+# Appliance Processing
+################################################################################
+
+class Appliance
+    attr_accessor :files
+
+    def initialize(body)
+        @body   = body
+        @source = @body["source"]
+        @files  = @body["files"] || Array.new
+    end
+
+    def temp_dir
+        return @temp_dir if @temp_dir
+
+        FileUtils.mkdir_p(CONF[:temp_dir])
+        @temp_dir = Dir.mktmpdir(nil, CONF[:temp_dir])
+    end
+
+    def download_cmd
+        if !defined?(@curl_exists) or !defined?(@wget_exists)
+            which_curl = `which curl`
+            @curl_exists = $?.exitstatus == 0
+
+            which_wget = `which wget`
+            @wget_exists = $?.exitstatus == 0
+        end
+
+        if @curl_exists
+            "curl -s #{@source}"
+        elsif @wget_exists
+            "wget -q #{@source} -O-"
+        else
+            fail("No curl or wget found.")
+        end
+    end
+
+    def to_hash
+        inherit_file_params = Hash.new
+
+        %w(os-id os-release os-arch hypervisor format).each do |p|
+            if (v = @body[p])
+                inherit_file_params[p] = v
+            end
+        end
+
+        {
+            "files" => @files.collect{|e| e.merge(inherit_file_params)},
+            "opennebula_template" => @body["opennebula_template"]
+        }
+    end
+end
+
+class OVA < Appliance
+    def unpack
+        pid, stdin, stdout, stderr = Open4.popen4("#{download_cmd} | tar -xf- -C #{temp_dir}")
+        _, status = Process::waitpid2 pid
+
+        if !status.success?
+            fail("Download error:\n#{stderr.read}")
+        end
+    end
+
+    def register_files
+        ovf_file = Dir["#{temp_dir}/*.ovf"][0]
+
+        ovf = OVFParserOpenNebula.new(ovf_file)
+        ovf.get_disks.each do |disk|
+            disk[:path]    = File.join(temp_dir, disk[:path])
+            appliance_file = ApplianceFile.register(disk)
+            @files << appliance_file.to_hash
+        end
+
+        @body["opennebula_template"] = ovf.to_one
+    end
+end
--- src/worker/lib/ovf_parser_opennebula.rb
+++ src/worker/lib/ovf_parser_opennebula.rb
@@ -14,7 +14,7 @@
 # limitations under the License.                                             #
 #--------------------------------------------------------------------------- #
 
-require "OVFParser"
+require "ovf_parser"
 require "erb"
 
 class OVFParserOpenNebula < OVFParser
