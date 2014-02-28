6ad49488f0e350b15be7f8ffb754f3cfe81aa4c9
Jaime Melis <jmelis@opennebula.org>
2014-02-28 15:05:09 +0100

    Don't try to iterate over the files if key is not defined

--- src/worker/lib/appliance_handler.rb
+++ src/worker/lib/appliance_handler.rb
@@ -147,7 +147,8 @@ class ApplianceHandler
 
     UUID_REGEX = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/
     def delete_files
-        @body["files"].each do |file|
+        files = @body["files"] || []
+        files.each do |file|
             uuid = File.basename(file["url"])
             if uuid.match(UUID_REGEX)
                 path = File.join(AppMarket::CONF[:repo],uuid)
