f3c92072f805593277c9b18b3ecf07c3a803d5b6
Jaime Melis <jmelis@opennebula.org>
2014-04-21 15:42:09 +0200

    Create a script that generates all packages

---
+++ gen_all_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export VERSION=${VERSION:-1.9.85}
+export SCRIPTS_DIR=$(cd `dirname $0`; pwd)/packages
+
+rm -rf  $SCRIPTS_DIR; mkdir -p $SCRIPTS_DIR
+
+PACKAGE_TYPE=rpm ./gen_package.sh
+PACKAGE_TYPE=deb ./gen_package.sh
+PACKAGE_TYPE=rpm ./gen_package.sh worker
+PACKAGE_TYPE=deb ./gen_package.sh worker
